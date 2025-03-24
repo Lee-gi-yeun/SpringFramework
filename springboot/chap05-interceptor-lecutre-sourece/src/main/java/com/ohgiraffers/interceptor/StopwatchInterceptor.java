cb097783052dfea4dc32d0e5c045e97e6a741686
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-03-24 11:12:00 +0900

    interceptor

--- springboot/chap05-interceptor-lecutre-sourece/src/main/java/com/ohgiraffers/interceptor/StopwatchInterceptor.java
+++ springboot/chap05-interceptor-lecutre-sourece/src/main/java/com/ohgiraffers/interceptor/StopwatchInterceptor.java
@@ -2,12 +2,21 @@ package com.ohgiraffers.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
+@RequiredArgsConstructor
 public class StopwatchInterceptor implements HandlerInterceptor {
+
+    // Interceptor는 스프링 컨테이너에 존재하는 빈을 의존성 주입 받을 수 있다
+    private final MenuService menuService;
+
+//    public StopwatchInterceptor(MenuService menuService) {
+//        this.menuService = menuService;
+//    }
 
     // 전처리 메소드
     @Override
@@ -32,5 +41,6 @@ public class StopwatchInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion 메소드 호출");
+        menuService.method();
     }
 }
