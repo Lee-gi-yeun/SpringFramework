233a3ed4e854747ed13addb5f84fc8af04e8f7ef
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-03-25 12:46:56 +0900

    mybaits java-ver

---
+++ mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/SelectBuilderService.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section02.provider;
+
+import com.ohgiraffers.common.SearchCriteria;
+
+public class SelectBuilderService {
+    public void testSimpleStatement() {
+
+    }
+
+    public void testDynamicStatement(SearchCriteria searchCriteria) {
+
+    }
+}
---
+++ mybatis/chap03-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/SqlBuilderService.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section02.provider;
+
+import com.ohgiraffers.common.MenuDTO;
+
+public class SqlBuilderService {
+    public void registMenu(MenuDTO menuDTO) {
+
+    }
+
+    public void modifyMenu(MenuDTO menuDTO) {
+
+    }
+
+    public void deleteMenu(int menuCode) {
+
+    }
+}
