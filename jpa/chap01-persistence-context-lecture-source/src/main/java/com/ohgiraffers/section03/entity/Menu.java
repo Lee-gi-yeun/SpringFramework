626f181ae44064204b504bf8ef239a7d4420cddf
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-03-27 18:17:05 +0900

    jpa java -ver

--- jpa/chap01-persistence-context-lecture-source/src/main/java/com/ohgiraffers/section03/entity/Menu.java
+++ jpa/chap01-persistence-context-lecture-source/src/main/java/com/ohgiraffers/section03/entity/Menu.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 public class Menu {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "menu_code")
     private int menuCode;
 
