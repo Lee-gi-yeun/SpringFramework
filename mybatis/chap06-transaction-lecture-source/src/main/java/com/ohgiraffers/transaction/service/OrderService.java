19c453c74e2b7bee0ecc8c5929d154e9eb6de740
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-03-27 10:41:31 +0900

    mybatis spring-ver

--- mybatis/chap06-transaction-lecture-source/src/main/java/com/ohgiraffers/transaction/service/OrderService.java
+++ mybatis/chap06-transaction-lecture-source/src/main/java/com/ohgiraffers/transaction/service/OrderService.java
@@ -7,23 +7,27 @@ import com.ohgiraffers.transaction.dto.OrderMenuDTO;
 import com.ohgiraffers.transaction.mapper.OrderMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderService {
 
     private final OrderMapper orderMapper;
 
-//    public OrderService(OrderMapper orderMapper) {
-//        this.orderMapper = orderMapper;
-//    }
+    public OrderService(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
 
     // 주문 등록 기능 메소드
     // Controller에서 넘어온 DTO를 도메인 모델로 변환한 후
     // 주문 정보(tbl_order)와 주문별 메뉴 정보(tbl_order_menu)를 하나의 트랜잭션으로 처리한다
+    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
     public void registerOrder(OrderDTO orderDTO, List<OrderMenuDTO> orderMenuDTOs){
         // DTO -> Domain 변환 (Order)
         Order order = new Order();
