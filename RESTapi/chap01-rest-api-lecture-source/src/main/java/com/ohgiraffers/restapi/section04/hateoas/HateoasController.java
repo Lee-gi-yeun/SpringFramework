6775f693ec2d000f51a19baa055eaa1abffb4d82
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-04-02 12:37:13 +0900

    REST API

--- RESTapi/chap01-rest-api-lecture-source/src/main/java/com/ohgiraffers/restapi/section04/hateoas/HateoasController.java
+++ RESTapi/chap01-rest-api-lecture-source/src/main/java/com/ohgiraffers/restapi/section04/hateoas/HateoasController.java
@@ -1,25 +1,32 @@
-package com.ohgiraffers.restapi.section02.responseentity;
+package com.ohgiraffers.restapi.section04.hateoas;
 
+import com.ohgiraffers.restapi.section02.responseentity.ResponseMessage;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
-@RequestMapping("/entity")
-public class ResponseEntityController {
+@RequestMapping("/hateoas")
+public class HateoasController {
 
     private List<UserDTO> users;
 
-    public ResponseEntityController(){
+    public HateoasController(){
         users = new ArrayList<>();
         users.add(new UserDTO(1,"user01", "pass01", "유관순"));
         users.add(new UserDTO(2,"user02", "pass02", "홍길동"));
@@ -28,20 +35,24 @@ public class ResponseEntityController {
 
     @GetMapping("/users")
     public ResponseEntity<ResponseMessage> findAllUsers(){
-        // 응답 헤더 설정 : JSON 응답이 default 이기는 하나 변경이 필요할 경우 HttpHeaders 설정 변경
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(
-                new MediaType("application", "json", StandardCharsets.UTF_8)
-        );
+
+        // Hateoas 설정
+        List<EntityModel<UserDTO>> usersWithRel = users.stream().map(
+                user -> EntityModel.of(
+                        user,
+                        linkTo(methodOn(HateoasController.class).findUserByNo(user.getNo())).withSelfRel(),
+                        linkTo(methodOn(HateoasController.class).findAllUsers()).withRel("users")
+                )
+        ).toList();
 
         // 응답 바디 설정
         Map<String, Object> responseMap = new HashMap<>();
-        responseMap.put("users", users);
+        responseMap.put("users", usersWithRel);
 
         // 응답 메세지 설정
         ResponseMessage responseMessage = new ResponseMessage(200,"조회 성공", responseMap);
 
-        return new ResponseEntity<>(responseMessage, httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 
     @GetMapping("/users/{userNo}")
@@ -65,43 +76,5 @@ public class ResponseEntityController {
                 .ok()
                 .headers(httpHeaders)
                 .body(new ResponseMessage(200, "조회성공", responseMap));
-    }
-
-    @PostMapping("/users")
-    public ResponseEntity<Void> registUser(@RequestBody UserDTO userDTO){
-
-        int lastUserNo = users.get(users.size()-1).getNo();
-        userDTO.setNo(lastUserNo+1);
-        users.add(userDTO);
-
-        return ResponseEntity
-                .created(URI.create("/entity/users/" + users.get(users.size() -1).getNo()))
-                .build();
-    }
-
-    @PutMapping("/users/{userNo}")
-    public ResponseEntity<Void> modifyUser(@PathVariable int userNo, @RequestBody UserDTO userDTO){
-        UserDTO foundUser = users
-                .stream()
-                .filter(user -> user.getNo() == userNo)
-                .findFirst()
-                .get();
-
-        foundUser.setPwd(userDTO.getPwd());
-        foundUser.setName(userDTO.getName());
-
-        return ResponseEntity.created(URI.create("/entity/users/" + userNo)).build();
-    }
-
-    @DeleteMapping("/users/{userNo}")
-    public ResponseEntity<Void> removeUser(@PathVariable int userNo){
-        UserDTO foundUser = users
-                .stream()
-                .filter(user -> user.getNo() == userNo)
-                .findFirst()
-                .get();
-        users.remove(foundUser);
-
-        return ResponseEntity.noContent().build();
     }
 }
