2b6764495c86197d300a460baa9a8323555bd668
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-04-03 11:30:48 +0900

    REST API-cqrs

--- RESTapi/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/query/controller/ProductQueryController.java
+++ RESTapi/chap02-cqrs-lecture-source/src/main/java/com/ohgiraffers/cqrs/product/query/controller/ProductQueryController.java
@@ -1,13 +1,17 @@
 package com.ohgiraffers.cqrs.product.query.controller;
 
 import com.ohgiraffers.cqrs.common.dto.ApiResponse;
+import com.ohgiraffers.cqrs.product.query.dto.request.ProductSearchRequest;
 import com.ohgiraffers.cqrs.product.query.dto.response.ProductDetailResponse;
+import com.ohgiraffers.cqrs.product.query.dto.response.ProductListResponse;
 import com.ohgiraffers.cqrs.product.query.service.ProductQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +22,13 @@ public class ProductQueryController {
     @GetMapping("/products/{productCode}")
     public ResponseEntity<ApiResponse<ProductDetailResponse>> getProduct(@PathVariable Long productCode) {
         ProductDetailResponse response = productQueryService.getProduct(productCode);
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<ApiResponse<ProductListResponse>> getProducts(ProductSearchRequest productSearchRequest) {
+        ProductListResponse response = productQueryService.getProducts(productSearchRequest);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
